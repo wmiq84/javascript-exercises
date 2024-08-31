7e623073a865f3ed81058710504bc042366a2201
wmiq84 <michaelwang3489@gmail.com>
2024-08-30 17:09:11 -0700

    Add code for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if (min < 0 || max < 0) return "ERROR";
+    else if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    else if (min > max) [min, max] = [max, min]
+    for (let i = min; i <= max; i++) {
+        sum += min;
+        min++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
