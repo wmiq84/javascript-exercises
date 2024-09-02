cea275d4779984b539b48f858f9b279917a4fdd7
wmiq84 <michaelwang3489@gmail.com>
2024-09-02 14:22:58 -0700

    Add code for calculator and palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+  return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, current) => current + sum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => current * sum, 1);
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) return 1;
+  let result = 1;
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const processedString = string
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join("");
+        
+
+    const reversedString = processedString
+        .split("")
+        .reverse()
+        .join("");
+        
+    return processedString === reversedString;
 };
 
 // Do not edit below this line
